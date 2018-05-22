a2094f7b11b8b138430ffd4388e2127dd63f232a
Rick Sherman <rick@shermdog.com>
2018-05-22 13:48:19 -0500

    Move to hiera backed examples

--- site/profile/manifests/arista_demo.pp
+++ site/profile/manifests/arista_demo.pp
@@ -1,47 +1,45 @@
-class profile::arista_demo {
+class profile::arista_demo (
+  Hash $interfaces = {},
+) {
 
-  package { 'rbeapi':
-    ensure   => 'installed',
-    provider => 'puppet_gem',
-  }
+  package { 'rbeapi':
+    ensure   => 'installed',
+    provider => 'puppet_gem',
+  }
 
-  eos_ipinterface { 'Ethernet1':
-    ensure  => 'present',
-    address => '1.1.1.1/24',
-    mtu     => '1500',
-  }
+  $interfaces.each |$interface, $parameters| {
+    eos_ipinterface { $interface:
+      ensure  => 'present',
+      address => $parameters[ipaddress],
+      mtu     => $parameters[mtu],
+    }
+  }
 
-  eos_ipinterface { 'Ethernet2':
-    ensure  => 'present',
-    address => '2.2.2.2/24',
-    mtu     => '1500',
-  }
+  eos_interface { 'Loopback1':
+    ensure => 'present',
+    enable => true,
+  }
 
-  eos_interface { 'Loopback1':
-    ensure => 'present',
-    enable => 'true',
-  }
-  
-  eos_vxlan { 'Vxlan1':
-    source_interface => 'Loopback1',
-    udp_port => 5500,
-    require => Eos_interface['Loopback1'],
-  }
+  eos_vxlan { 'Vxlan1':
+    source_interface => 'Loopback1',
+    udp_port         => 5500,
+    require          => Eos_interface['Loopback1'],
+  }
 
-  eos_bgp_config { '65001':
-    ensure             => present,
-    enable             => true,
-    router_id          => '192.0.2.4',
-    maximum_paths      => 8,
-    maximum_ecmp_paths => 8,
-  }
-  
-  eos_bgp_neighbor { '192.0.3.1':
-    ensure         => present,
-    enable         => true,
-    peer_group     => 'Edge',
-    remote_as      => 65004,
-    send_community => 'enable',
-    next_hop_self  => 'enable',
-  }
+  eos_bgp_config { '65001':
+    ensure             => present,
+    enable             => true,
+    router_id          => '192.0.2.4',
+    maximum_paths      => 8,
+    maximum_ecmp_paths => 8,
+  }
+
+  eos_bgp_neighbor { '192.0.3.1':
+    ensure         => present,
+    enable         => true,
+    peer_group     => 'Edge',
+    remote_as      => 65004,
+    send_community => 'enable',
+    next_hop_self  => 'enable',
+  }
 }
--- site/profile/manifests/cisco_demo.pp
+++ site/profile/manifests/cisco_demo.pp
@@ -1,93 +1,64 @@
 class profile::cisco_demo (
-  String $int1_ipaddress        = '172.16.1.1',
-  Integer $int1_netmask         = 24,
-  Integer $int1_mtu             = 1500,
-  Boolean $int1_enable          = false,
-  String $int2_ipaddress        = '172.16.2.2',
-  Integer $int2_netmask         = 24,
-  Integer $int2_mtu             = 1500,
-  Boolean $int2_enable          = false,
-  String $int3_ipaddress        = '172.16.3.3',
-  Integer $int3_netmask         = 24,
-  Integer $int3_mtu             = 1500,
-  Boolean $int3_enable          = false,
-  String $int4_ipaddress        = '172.16.4.4',
-  Integer $int4_netmask         = 24,
-  Integer $int4_mtu             = 1500,
-  Boolean $int4_enable          = false
+  Hash $interfaces = {},
+  Hash $vlans = {},
 ) {
 
   include ciscopuppet::install
 
-  cisco_interface { 'ethernet1/1':
-    ensure                                         => 'present',
-    ipv4_address                                   => $int1_ipaddress,
-    ipv4_netmask_length                            => $int1_netmask,
-    mtu                                            => $int1_mtu,
-    shutdown                                       => $int1_enable,
-    switchport_mode                                => 'disabled',
-  }
+  $vlans.each |$vlan, $parameters| {
+    cisco_vlan { $vlan:
+      ensure    => $parameters[ensure],
+      shutdown  => $parameters[shutdown],
+      state     => $parameters[state],
+      vlan_name => $parameters[vlan_name],
+    }
+  }
 
-cisco_interface { 'ethernet1/2':
-    ensure                                         => 'present',
-    ipv4_address                                   => $int2_ipaddress,
-    ipv4_netmask_length                            => $int2_netmask,
-    mtu                                            => $int2_mtu,
-    shutdown                                       => $int2_enable,
-    switchport_mode                                => 'disabled',
-  }
+  $interfaces.each |$interface, $parameters| {
+    cisco_interface { $interface:
+      ensure              => 'present',
+      ipv4_address        => $parameters[ipaddress],
+      ipv4_netmask_length => $parameters[netmask],
+      mtu                 => $parameters[mtu],
+      shutdown            => $parameters[shutdown],
+      access_vlan         => $parameters[access_vlan],
+      switchport_mode     => $parameters[switchport_mode],
+    }
+  }
 
-cisco_interface { 'ethernet1/3':
-    ensure                                         => 'present',
-    ipv4_address                                   => $int3_ipaddress,
-    ipv4_netmask_length                            => $int3_netmask,
-    mtu                                            => $int3_mtu,
-    shutdown                                       => $int3_enable,
-    switchport_mode                                => 'disabled',
-  }
+  cisco_interface { 'loopback1':
+    ensure              => 'present',
+    description         => 'Puppet FTW',
+    ipv4_address        => '3.3.3.3',
+    ipv4_netmask_length => '24',
+  }
 
-cisco_interface { 'ethernet1/4':
-    ensure                                         => 'present',
-    ipv4_address                                   => $int4_ipaddress,
-    ipv4_netmask_length                            => $int4_netmask,
-    mtu                                            => $int4_mtu,
-    shutdown                                       => $int4_enable,
-    switchport_mode                                => 'disabled',
-  }
+  cisco_vxlan_vtep { 'nve1':
+    ensure            => present,
+    description       => 'Configured by puppet',
+    host_reachability => 'evpn',
+    shutdown          => false,
+    source_interface  => 'loopback1',
+  }
 
-  cisco_interface { 'loopback1':
-    ensure                                         => 'present',
-    description                                    => 'Puppet FTW',
-    ipv4_address                                   => '3.3.3.3',
-    ipv4_netmask_length                            => '24',
-  }
+  cisco_vxlan_vtep_vni {'nve1 10000':
+    ensure              => present,
+    assoc_vrf           => false,
+    ingress_replication => 'static',
+    peer_list           => ['4.4.4.4', '5.5.5.5'],
+  }
 
-  cisco_vxlan_vtep { 'nve1':
-    ensure                          => present,
-    description                     => 'Configured by puppet',
-    host_reachability               => 'evpn',
-    shutdown                        => 'false',
-    source_interface                => 'loopback1',
-  }
 
-  cisco_vxlan_vtep_vni {'nve1 10000':
-    ensure              => present,
-    assoc_vrf           => false,
-    ingress_replication => 'static',
-    peer_list           => ['4.4.4.4', '5.5.5.5'],
-  }
+  cisco_bgp { '65001 default':
+    ensure      => 'present',
+    maxas_limit => '8',
+    router_id   => '192.0.2.4',
+    shutdown    => false,
+  }
 
-  cisco_bgp { '65001 default':
-    ensure        => 'present',
-    maxas_limit   => '8',
-    router_id     => '192.0.2.4',
-    shutdown      => 'false',
-  }
-
-  cisco_bgp_neighbor { '65001 default 192.0.3.1':
-    ensure                 => 'present',
-    remote_as              => '65004',
-    require                => Cisco_bgp['65001 default'],
-  }
-
+  cisco_bgp_neighbor { '65001 default 192.0.3.1':
+    ensure    => 'present',
+    remote_as => '65004',
+    require   => Cisco_bgp['65001 default'],
+  }
 }
